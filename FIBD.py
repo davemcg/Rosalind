d4f2bad0835c410e6030ca59288f2686b97f8ef0
davemcg <mcgaughey@gmail.com>
2015-03-13 15:50:44 -0400

    Finally got it working after dumping numpy array

--- FIBD.py
+++ FIBD.py
@@ -3,28 +3,20 @@
 """
 Given: Positive integers n≤100 and m≤20.
 
-Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
+Return: The total number of pairs of rabbits 
+that will remain after the n-th month if all 
+rabbits live for m months.
 """
 
 import fileinput
-import numpy as np
-import sys
 
-def fibd(n, k):
-	n = int(n)
-	k = int(k)
-	fertile = 0
-#	pop = np.zeros(k, dtype = np.float64)
-	pop = np.zeros(k)
-	for i in range(n):
-		if i <= 0:
-			pop[0] = 1
-		else:
-			fertile = np.sum(pop[1:])
-			pop = np.roll(pop,1)
-			pop[0] = fertile
-	return(np.sum(pop))
+def fibd(n, m):
+	pop = [1] + [0]*(m-1)
+	for i in range(n-1):
+		fertile = sum(pop[1:])
+		pop = [fertile] + pop[:-1]
+	return(sum(pop))
 
 for line in fileinput.input():
-	n, k = line.split()
-	print(format(fibd(n,k), '.20f'))
+	n, m = line.split()
+	print(fibd(int(n),int(m)))
